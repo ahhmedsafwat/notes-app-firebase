8e9f6f8b15f12c6f7e8c672b6093604af6f5574d
ahmedsafwat <a01273477051@gmail.com>
2023-05-18 08:58:22 +0300

    checks for the user current state (⊙_⊙)？

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/firebase_options.dart';
@@ -7,9 +8,18 @@ import 'auth/signup.dart';
 import 'cred/add_notes.dart';
 import 'home/home_page.dart';
 
+bool isLogin = false;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final userstate = FirebaseAuth.instance.currentUser;
+  if (userstate == null) {
+    isLogin;
+  } else {
+    isLogin = true;
+  }
+
   runApp(const MyApp());
 }
 
@@ -20,7 +30,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: Login.login,
+      initialRoute: isLogin ? HomePage.homePage : Login.login,
       routes: {
         Login.login: (context) {
           return const Login();
